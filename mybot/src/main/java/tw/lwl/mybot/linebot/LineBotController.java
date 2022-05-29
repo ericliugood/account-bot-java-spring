8d3afddb24fd3b23cd70f32c5b2caa417d842281
lwl <ericliubig@gmail.com>
2022-05-29 11:39:01 +0800

    line bot
    line bot

---
+++ mybot/src/main/java/tw/lwl/mybot/linebot/LineBotController.java
@@ -0,0 +1,11 @@
+package tw.lwl.mybot.linebot;
+
+import com.linecorp.bot.client.LineBlobClient;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+
+
+@LineMessageHandler
+public class LineBotController {
+
+}
