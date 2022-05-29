1fd48be1c52d4dba32b2fb1d0e285a40ac281f39
lwl <ericliubig@gmail.com>
2022-05-29 13:05:54 +0800

    can reply
    can reply

--- mybot/src/main/java/tw/lwl/mybot/linebot/LineBotController.java
+++ mybot/src/main/java/tw/lwl/mybot/linebot/LineBotController.java
@@ -1,11 +1,88 @@
 package tw.lwl.mybot.linebot;
 
 import com.linecorp.bot.client.LineBlobClient;
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import com.linecorp.bot.model.ReplyMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import lombok.NonNull;
+import static java.util.Collections.singletonList;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 
 @LineMessageHandler
 public class LineBotController {
+
+    @Autowired
+    private LineMessagingClient lineMessagingClient;
+
+    @Autowired
+    private LineBlobClient lineBlobClient;
+
+    int sections=0;
+
+
+    @EventMapping
+    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+
+        final String originalMessageText = event.getMessage().getText();
+        TextMessage a ;
+
+        a=TextMessage.builder().text("555").build();
+
+        reply(event.getReplyToken(), a);
+
+
+
+
+        return new TextMessage(originalMessageText);
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
+
+    private void reply(@NonNull String replyToken, @NonNull Message message) {
+        reply(replyToken, singletonList(message));
+    }
+
+    private void reply(@NonNull String replyToken, @NonNull List<Message> messages) {
+        reply(replyToken, messages, false);
+    }
+
+    private void reply(@NonNull String replyToken,
+                       @NonNull List<Message> messages,
+                       boolean notificationDisabled) {
+        try {
+            BotApiResponse apiResponse = lineMessagingClient
+                    .replyMessage(new ReplyMessage(replyToken, messages, notificationDisabled))
+                    .get();
+
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void replyText(@NonNull String replyToken, @NonNull String message) {
+        if (replyToken.isEmpty()) {
+            throw new IllegalArgumentException("replyToken must not be empty");
+        }
+        if (message.length() > 1000) {
+            message = message.substring(0, 1000 - 2) + "……";
+        }
+        this.reply(replyToken, new TextMessage(message));
+    }
+
+
 
 }
